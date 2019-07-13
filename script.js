7abaa72e6c3c32273e625799c767153df91dba55
Maria-khurshid <48955107+Maria-khurshid@users.noreply.github.com>
2019-07-13 13:05:27 +0500

    more details will be added

---
+++ script.js
@@ -0,0 +1,92 @@
+let restourent = {
+    name: "Macopolo",
+    city: "Karachi",
+    country: "Pakistan",
+    shortDescription: " $$$$,Pakistani, International, Vegetarian Friendly",
+    reviews: "93%",
+    hasMap: true,
+    hasWebsite: true,
+    rating: "4.5",
+    scope: "Certificate of Excellence2018 - 2019 Winner",
+    details: [{
+            cuisines: "Pakistani, International"
+        },
+        {
+            specialDiets: "Vegetarian Friendly, Halal, Vegan Options, Gluten Free Options"
+        },
+        {
+            meals: "Breakfast, Lunch, Dinner, Brunch"
+        },
+        {
+            features: "Reservations, Seating, Parking Available, Validated Parking, Highchairs Available, Wheelchair Accessible, Table Service"
+        }
+    ],
+    generalInformation: [{
+            phone: "(021) 111 505 505"
+        },
+        {
+            website: "http://www.pchotels.com/PCHK/dining.php"
+        },
+        {
+            location: "Pearl Continental Hotel, Club Road"
+        },
+        {
+            additionalAddress: ["Karachi, Pakistan", "city/town, state/province/region"]
+        }
+    ],
+    travelerRating: [{
+        excellent: 52,
+        veryGood: 24,
+        average: 11,
+        poor: 4,
+        terrible: 1
+    }],
+    foodRating: [{
+            name: "Halwa",
+            rating: "delicious"
+        },
+        {
+            name: "nihari",
+            rating: "delicious"
+        },
+        {
+            name: "marco polo",
+            rating: "delicious"
+        },
+        {
+            name: "hi tea",
+            rating: "tasty"
+        },
+
+        {
+            name: "high tea",
+            rating: "tasty"
+        },
+
+        {
+            rating: "excellent breakfast"
+        },
+        {
+            rating: "vast Varity"
+        },
+        {
+            name: "pc karachi"
+        },
+        {
+            name: "manager mr"
+        },
+        {
+            name: "international dishes"
+        },
+        {
+            name: "peal continental hotal"
+        },
+        {
+            rating: "amazing experiences"
+        },
+        {
+            name: "service was excellent"
+        },
+
+    ]
+};
